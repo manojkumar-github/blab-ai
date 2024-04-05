725de572783b50d6d73b20a3560e704e311e95b2
MJ <manoj.kumar31028@gmail.com>
2024-04-05 17:50:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ import pandas as pd
 import streamlit as st
 
 st.title('M-AI')
+
+prompt: str = st.chat_input("Enter a prompt here")
+
+USER = "user"
+ASSISTANT = "assistant"
+
+if prompt:
+    st.chat_message(USER).write(prompt)
+    st.chat_message(ASSISTANT).write(f"You wrote {prompt}")

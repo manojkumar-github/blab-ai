ba304019e6f39bd70ee7d74b1b49846a659c8394
MJ <manoj.kumar31028@gmail.com>
2024-04-06 22:28:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
-import pandas as pd
-import streamlit as st
-import requests
+from dataclasses import dataclass
 
-st.title('Resh-AI')
+import streamlit as st
+from vertexai.preview.language_models import TextGenerationModel
+
+
+@dataclass
+class Message:
+    actor: str
+    payload: str
+
+
+def get_llm() -> TextGenerationModel:
+    return TextGenerationModel.from_pretrained("text-bison@001")
+
+
+USER = "user"
+ASSISTANT = "ai"
+MESSAGES = "messages"
+if MESSAGES not in st.session_state:
+    st.session_state[MESSAGES] = [Message(actor=ASSISTANT, payload="Hi!How can I help you?")]
+
+msg: Message
+for msg in st.session_state[MESSAGES]:
+    st.chat_message(msg.actor).write(msg.payload)
 
 prompt: str = st.chat_input("Enter a prompt here")
 
-USER = "user"
-ASSISTANT = "assistant"
-
 if prompt:
+    st.session_state[MESSAGES].append(Message(actor=USER, payload=prompt))
     st.chat_message(USER).write(prompt)
-    message = requests.get('https://blab-backend.onrender.com/')
-    st.chat_message(ASSISTANT).write(f"Reshma AI wrote {message.content}")
+    response: str = get_llm().predict(prompt=prompt).text
+    st.session_state[MESSAGES].append(Message(actor=ASSISTANT, payload=response))
+    st.chat_message(ASSISTANT).write(response)
